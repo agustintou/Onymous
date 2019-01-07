4ee972802c1d4a045e932b0e947a0464d4ec2ea9
Agustin Touceda Papa <agustin_touceda@hotmail.com>
2019-01-07 16:29:11 -0300

    Locator agregado

---
+++ Onymous 1.0/OnymousMobile/OnymousMobile/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,14 @@
+﻿using OnymousMobile.ViewModels;
+
+namespace OnymousMobile.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
---
+++ Onymous 1.0/OnymousMobile/OnymousMobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace OnymousMobile.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Builders
+        public MainViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
